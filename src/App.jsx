4ac408533cafe71e695174c14f1803e8eac3b6a1
saurabhoatstech <saurabhnegi0007@gmail.com>
2023-11-25 21:49:48 +0530

    router setup added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
+import Body from "./components/Body";
+
 function App() {
-  return (
-    <>
-      <p className="text-xl text-red-500">App</p>
-    </>
-  );
+  return <Body />;
 }
 
 export default App;
